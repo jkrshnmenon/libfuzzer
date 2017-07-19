326be5d786d98c87761ccf33509101be8eabecd7
Jayakrishna Menon <jkrshnmenon@gmail.com>
2017-07-19 18:36:37 +0530

    compiled telnet as library in fuzzer function

--- inetutils-1.9/ping/test.cpp
+++ inetutils-1.9/ping/test.cpp
@@ -1,10 +1,15 @@
-extern "C"{
-#include "ping.h"
-}
+#include<iostream>
+#include<stdlib.h>
+#include<stdint.h>
+#include<string.h>
+
+extern "C" void decode_pattern(const char *text, int *pattern_len, unsigned char *pattern_data);
 
 extern "C" int LLVMFuzzerTestOneInput( const uint8_t *Data, size_t Size)
 {
-    double a = 0.0;
-    nabs(a);
+    char *ptr = (char *)malloc(Size);
+    memcpy(ptr, Data, Size);
+    int size = (int)Size;
+    decode_pattern((const char *)ptr, &size, (unsigned char *)Data);
     return 0;
 }
---
+++ inetutils-1.9/telnet/test.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+
+extern "C" void tninit(void);
+
+int main(){
+    tninit();
+    return 0;
+}
